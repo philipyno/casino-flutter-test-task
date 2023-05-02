debe2701ad711da804469dacd4e76d90700e9048
Eminokanju Oghenekparobor <66335632+oghenekparobor@users.noreply.github.com>
2023-05-02 11:03:10 +0100

    chore: added the remote DS class

--- lib/src/data/datasource/remote/remote.dart
+++ lib/src/data/datasource/remote/remote.dart
@@ -1,3 +1,11 @@
 abstract class RemoteDS {
   Future<dynamic> fetch();
 }
+
+class RemoteDSImpl extends RemoteDS {
+  @override
+  Future fetch() {
+    // TODO: implement fetch
+    throw UnimplementedError();
+  }
+}
